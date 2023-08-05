5fada19ec3e893d3bd2ae50afbbd58d97859037b
Kenol David <kenoldavid.git@gmail.com>
2023-08-05 16:25:55 -0400

    lb to kg - procedural implementation.

---
+++ SergeySkudaevCppExamples/Convert/src/convert.cpp
@@ -0,0 +1,25 @@
+// Convert.cpp 
+// Converts pounds to kilograms in a procedural way
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	double lb;
+	double kg;
+	double c;
+
+	std::cout << "Converting Pounds to Kilograms" << std::endl;
+	std::cout << "\nPlease enter a mass in pounds (lbs):";
+	std::cin >> lb;
+
+	c = 0.454;
+	kg = c * lb;
+
+	std::cout << lb << "lb = " << kg << "kg " << std::endl;
+
+	int hold = 1;
+	std::cin >> hold;
+
+	return 0;
+}
